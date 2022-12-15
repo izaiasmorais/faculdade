251542d7e1d6c8fd2041d4ebe990eda0415e5fab
Izaías Lima <izaiaslima356@gmail.com>
2022-12-15 00:17:58 -0300

    chore: bhaskara formula calc added

---
+++ beecrowd/bhaskaraFormula.c
@@ -0,0 +1,22 @@
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  double A, B, C, x1, x2, delta;
+
+  scanf("%lf %lf %lf", &A, &B, &C);
+
+  delta = B * B - 4 * A * C;
+  x1 = ((-B) + (sqrt(delta))) / (2 * A);
+  x2 = ((-B) - (sqrt(delta))) / (2 * A);
+
+  if (delta < 0 || A == 0) {
+    printf("Impossivel calcular\n");
+  } else {
+    printf("R1 = %.5lf\n", x1);
+    printf("R2 = %.5lf\n", x2);
+  }
+
+  return 0;
+}
