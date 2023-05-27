da32c83a3266988a8fd5647a1f1c06a6ac3f3714
Izaias <izaiaslima356@gmail.com>
2023-05-27 07:46:01 -0300

    chore: set up list task

---
+++ structures/atividade_lista_encadeada/java/Main.java
@@ -0,0 +1,7 @@
+package atividade_lista_encadeada.java;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
--- structures/moda/Moda.java
+++ structures/moda/Moda.java
@@ -2,7 +2,7 @@ package moda;
 
 import java.util.Scanner;
 
-public class Moda {
+public class moda {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
